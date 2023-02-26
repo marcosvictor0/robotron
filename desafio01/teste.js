8c0955efa345b490de18f6249fc41cf7c9abf506
Marcos Victor <marcos_victor159@hotmail.com>
2023-02-26 18:21:02 -0300

    desafio feito da alura, (acessando um botão para enviar uma mensagem através do click)

---
+++ desafio01/teste.js
@@ -0,0 +1,11 @@
+// const enviarNotas = document.getElementById("calcular")
+// enviarNotas.addEventListener("click", fuiclicado)
+
+// function fuiclicado() {
+//     console.log("Fui Clicado!")
+// }
+
+
+document.getElementById("calcular").addEventListener("click", ()=>{
+    console.log("Fui Clicado!!!")
+})
