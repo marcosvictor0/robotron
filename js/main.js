b761f638757bd14cc8d43be135a9a289a51770af
Marcos Victor <marcos_victor159@hotmail.com>
2023-02-26 18:57:50 -0300

    adc o evento de somar e subtrair a quantidade de braços através de funções do js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
-const robotronDizendoOla = document.getElementById("robotron")
+// const robotronDizendoOla = document.getElementById("robotron")
 
-robotronDizendoOla.addEventListener("click", ()=> {
-    console.log("Esse Olá foi feita pela função anônima.")
+// robotronDizendoOla.addEventListener("click", ()=> {
+//     console.log("Esse Olá foi feita pela função anônima.")
+// })
+
+// /*
+// a função addEventListener sempre esperar receber 2 coisas.
+
+// a primeira é o que ela vai fazer, qual operação.
+// a segunda é 
+// */
+
+// function dizerOla(nome) {
+//     console.log("Olá", nome)
+// }
+
+// dizerOla('Marcos')
+
+const subtrair = document.getElementById("subtrair")
+const somar = document.getElementById("somar")
+const braco = document.getElementById("braco")
+
+somar.addEventListener("click", (evento)=> {
+  let valor = parseInt(braco.value) 
+    braco.value = valor + 1
+})  
+
+subtrair.addEventListener("click", (evento)=> {
+    let valor = parseInt(braco.value)
+    braco.value = valor - 1
 })
-
-/*
-a função addEventListener sempre esperar receber 2 coisas.
-
-a primeira é o que ela vai fazer, qual operação.
-a segunda é 
-*/
-
-function dizerOla(nome) {
-    console.log("Olá", nome)
-}
-
-dizerOla('Marcos')
